2ff91314f2858d81b6411720c00d0357f4fd92cf
ecrows <evan.of.crows@gmail.com>
2018-08-11 16:14:26 -0400

    Lots of dialog.  Functional memory counter.

---
+++ Scripts/MemoryCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCounter : MonoBehaviour
+{
+	public int memory = 2498;
+	public bool leaking = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (leaking) {
+			if (memory > 0) {
+				memory -= 1;
+			}
+		}
+	}
+}
+		
---
+++ Scripts/MemoryDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MemoryDisplay : MonoBehaviour
+{
+	public GameObject girl;
+	MemoryCounter mc;
+	Text txt;
+
+	// Use this for initialization
+	void Start () {
+		mc = girl.GetComponent <MemoryCounter>();
+		txt = gameObject.GetComponent<UnityEngine.UI.Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		txt.text = "Memory remaining: " + mc.memory + "TB";
+	}
+}
+
